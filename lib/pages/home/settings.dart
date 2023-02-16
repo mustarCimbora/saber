8bec70971e0bbde06c21257dd67966dbdc8af614
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-02-16 06:02:27 +0000

    prefs: use enums instead of int

--- lib/pages/home/settings.dart
+++ lib/pages/home/settings.dart
@@ -23,18 +23,6 @@ class SettingsPage extends StatefulWidget {
   @override
   State<SettingsPage> createState() => _SettingsPageState();
 
-  static final _shouldAlwaysAlertForUpdates = TransformedPref(
-    Prefs.updatesToIgnore,
-    (int value) => value <= 0,
-    (bool value) => value ? 0 : 1,
-  );
-
-  static final _editorToolbarAlignment = TransformedPref(
-    Prefs.editorToolbarAlignment,
-    (AxisDirection value) => value.index,
-    (int value) => AxisDirection.values[value],
-  );
-
   static Future<bool?> showResetDialog({
     required BuildContext context,
     required IPref pref,
@@ -65,6 +53,32 @@ class SettingsPage extends StatefulWidget {
       ),
     );
   }
+}
+
+abstract class _SettingsPrefs {
+  static final appTheme = TransformedPref(
+    Prefs.appTheme,
+    (ThemeMode value) => value.index,
+    (int value) => ThemeMode.values[value],
+  );
+
+  static final platform = TransformedPref(
+    Prefs.platform,
+    (TargetPlatform value) => value.index,
+    (int value) => TargetPlatform.values[value],
+  );
+
+  static final shouldAlwaysAlertForUpdates = TransformedPref(
+    Prefs.updatesToIgnore,
+    (int value) => value <= 0,
+    (bool value) => value ? 0 : 1,
+  );
+
+  static final editorToolbarAlignment = TransformedPref(
+    Prefs.editorToolbarAlignment,
+    (AxisDirection value) => value.index,
+    (int value) => AxisDirection.values[value],
+  );
 }
 
 class _SettingsPageState extends State<SettingsPage> {
@@ -172,7 +186,7 @@ class _SettingsPageState extends State<SettingsPage> {
                       if (i == ThemeMode.dark.index) return Icons.dark_mode;
                       return null;
                     },
-                    pref: Prefs.appTheme,
+                    pref: _SettingsPrefs.appTheme,
                     optionsWidth: 60,
                     options: [
                       ToggleButtonsOption(ThemeMode.system.index, Icon(Icons.brightness_auto, semanticLabel: t.settings.themeModes.system)),
@@ -187,19 +201,19 @@ class _SettingsPageState extends State<SettingsPage> {
                       if (platform == TargetPlatform.macOS) return Icons.apple;
                       return materialIcon;
                     },
-                    pref: Prefs.platform,
+                    pref: _SettingsPrefs.platform,
                     optionsWidth: 60,
                     options: [
                       ToggleButtonsOption(
                         () {
-                          if (usesMaterialByDefault) return -1;
+                          if (usesMaterialByDefault) return defaultTargetPlatform.index;
                           return TargetPlatform.android.index;
                         }(),
                         const Icon(materialIcon, semanticLabel: "Material"),
                       ),
                       ToggleButtonsOption(
                         () {
-                          if (!usesMaterialByDefault) return -1;
+                          if (!usesMaterialByDefault) return defaultTargetPlatform.index;
                           return TargetPlatform.iOS.index;
                         }(),
                         const Icon(Icons.apple, semanticLabel: "Cupertino"),
@@ -231,12 +245,12 @@ class _SettingsPageState extends State<SettingsPage> {
                 children: [
                   SettingsSelection(
                     title: t.settings.prefLabels.editorToolbarAlignment,
-                    subtitle: t.settings.axisDirections[SettingsPage._editorToolbarAlignment.value],
+                    subtitle: t.settings.axisDirections[_SettingsPrefs.editorToolbarAlignment.value],
                     iconBuilder: (num i) {
                       if (i is! int || i >= materialDirectionIcons.length) return null;
                       return cupertino ? cupertinoDirectionIcons[i] : materialDirectionIcons[i];
                     },
-                    pref: SettingsPage._editorToolbarAlignment,
+                    pref: _SettingsPrefs.editorToolbarAlignment,
                     optionsWidth: 60,
                     options: [
                       for (final AxisDirection direction in AxisDirection.values)
